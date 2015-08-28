1b021901737f22ef93dc2f1b7350be821ccd00d5
company <510420060@qq.com>
2015-08-28 18:17:23 +0800

    蓝牙的操作

--- bluetoothtest/src/main/java/com/zhd/bluetoothtest/DeviceActivity.java
+++ bluetoothtest/src/main/java/com/zhd/bluetoothtest/DeviceActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Toast;
 
 /**
@@ -11,9 +12,8 @@ import android.widget.Toast;
  */
 public class DeviceActivity extends Activity {
 
-    //定义蓝牙启动返回的内容
-    private static final int REQUEST_CONNCECT_DEVICE = 1;
-    private static final int REQUEST_ENABLE_BT = 2;
+    //创建一个handler来实现线程间通信
+    private Handler mhandler=new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,18 +21,4 @@ public class DeviceActivity extends Activity {
         setContentView(R.layout.activity_device);
     }
 
-    private void openBlueAdapter() {
-        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-        if (adapter == null) {
-            Toast.makeText(this, "该设备不支持蓝牙", Toast.LENGTH_SHORT).show();
-            return;
-        }
-        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        startActivityForResult(intent, REQUEST_ENABLE_BT);
-        adapter.enable();//打开蓝牙
-        adapter.disable();//关闭蓝牙
-        //打开本机的蓝牙被发现功能
-        Intent discoverableIntent=new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-        discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,300);//设置可以被检测的时间
-    }
 }
--- bluetoothtest/src/main/java/com/zhd/bluetoothtest/MainActivity.java
+++ bluetoothtest/src/main/java/com/zhd/bluetoothtest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.zhd.bluetoothtest;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -7,22 +8,38 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
+    //请求打开蓝牙
+    private static final int REQUEST_ENABLE = 0x1;
+
+    //请求能够被搜索
+    private static final int REQUEST_DISCOVERABLE=0x2;
+    //蓝牙适配器
+    private BluetoothAdapter adapter;
+    //按钮
+    Button btn_open,btn_search,btn_discover,btn_close,btn_client,btn_server;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btn_search= (Button) findViewById(R.id.search);
-        btn_search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent("com.zhd.device");
-                startActivity(intent);
-            }
-        });
+        //获得蓝牙适配器
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        if (adapter == null) {
+            Toast.makeText(this, "该设备不支持蓝牙", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        //对按钮添加监听
+        btn_open= (Button) findViewById(R.id.open);
+        btn_close= (Button) findViewById(R.id.close);
+        btn_discover= (Button) findViewById(R.id.discover);
+
+        btn_open.setOnClickListener(this);
+        btn_close.setOnClickListener(this);
+        btn_discover.setOnClickListener(this);
     }
 
     @Override
@@ -47,8 +64,36 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         return super.onOptionsItemSelected(item);
     }
 
+    /**
+     * 以下两种都可以打开蓝牙，但是通过intent打开的用户体验更好
+     * 直接使用适配器对象打开体验不好
+     */
     @Override
     public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.open:
+                intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(intent, REQUEST_ENABLE);
+                //adapter.enable();
+                intent=null;
+                break;
+            case R.id.discover:
+                intent=new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+                //这里设置蓝牙设备搜索的时间
+                intent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,300);
+                startActivityForResult(intent, REQUEST_DISCOVERABLE);
+                //adapter.cancelDiscovery();
+                intent=null;
+                break;
+            case R.id.close:
+                adapter.disable();
+                break;
+            case R.id.search:
+                intent=new Intent(this,DeviceActivity.class);
+                startActivity(intent);
+                break;
+        }
 
     }
 }
